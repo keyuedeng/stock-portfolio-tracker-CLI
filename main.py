4feffd724ba3b0bdd1b7799246dc4689cf67d643
keyuedeng <keyuedeng6@gmail.com>
2026-02-22 15:33:17 -0800

    add display summary function

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ from api_client import get_curr_price
 from storage import *
 from portfolio import *
 
-price, error = get_curr_price("AAPL")
-
-if error:
-    print(f"Error: {error}")
-else:
-    print(f"The current price of AAPL is ${price:.2f}")
 
 portfolio = load_portfolio()
-display_portfolio(portfolio)
 
 portfolio_value = calculate_portfolio_value(portfolio)
-print(portfolio_value)
+display_portfolio_summary(portfolio_value)
--- portfolio.py
+++ portfolio.py
@@ -52,3 +52,36 @@ def calculate_portfolio_value(portfolio_data):
         }
     
     return results
+
+def display_portfolio_summary(portfolio_value):
+    total_cost_basis = 0
+    total_curr_val = 0
+
+    for ticker, data in portfolio_value.items():
+        total_cost_basis += data['cost_basis']
+        total_curr_val += data['current_value']
+
+        # per ticker data
+        print(f"{ticker}:")
+        
+        print(f"\tTotal Shares: {data['total_shares']}")
+        print(f"\tCost basis: ${data['cost_basis']:.2f}")
+        print(f"\tCurrent Value: ${data['current_value']:.2f}")
+        if data["pnl_dollar"] < 0:
+            print(f"\tP&L: ${data['pnl_dollar']:.2f} ({data['pnl_percent']:.2f}%)")
+        else:
+            print(f"\tP&L: +${data['pnl_dollar']:.2f} (+{data['pnl_percent']:.2f}%)")
+
+    # calculate total pnl
+    total_pnl_dollar = total_curr_val - total_cost_basis
+    total_pnl_percent = 100 * total_pnl_dollar/total_cost_basis
+
+    print("\n")
+    print("-" * 30)
+    print("Total Portfolio:")
+    print(f"\tCost Basis: ${total_cost_basis:.2f}")
+    print(f"\tCurrent Value: ${total_curr_val:.2f}")
+    if total_pnl_dollar < 0:
+        print(f"\tTotal P&L: ${total_pnl_dollar:.2f} ({total_pnl_percent:.2f}%)")
+    else:
+        print(f"\tTotal P&L: +${total_pnl_dollar:.2f} (+{total_pnl_percent:.2f}%)")
